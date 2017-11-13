f4e497390d6e555a82b30a1e75c8138a1f5c4cf0
Andrew Belt <andrewpbelt@gmail.com>
2017-11-13 00:07:57 -0500

    Added website and manual URL

--- src/Tutorial.cpp
+++ src/Tutorial.cpp
@@ -11,6 +11,8 @@ void init(rack::Plugin *p) {
 #ifdef VERSION
 	p->version = TOSTRING(VERSION);
 #endif
+	p->website = "https://github.com/VCVRack/Tutorial";
+	p->manual = "https://github.com/VCVRack/Tutorial/blob/master/README.md";
 
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
 	p->addModel(createModel<MyModuleWidget>("Tutorial", "MyModule", "My Module", OSCILLATOR_TAG));
