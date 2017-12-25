a156f5d388de4a5199ebe5d35f1e588e013a7b71
Andrew Belt <andrewpbelt@gmail.com>
2017-12-25 00:23:58 -0500

    Remove condition around version

--- src/Tutorial.cpp
+++ src/Tutorial.cpp
@@ -11,9 +11,7 @@ void init(rack::Plugin *p) {
 	// To guarantee uniqueness, it is a good idea to prefix the slug by your name, alias, or company name if available, e.g. "MyCompany-MyPlugin".
 	// The ZIP package must only contain one folder, with the name equal to the plugin's slug.
 	p->slug = "Tutorial";
-#ifdef VERSION
 	p->version = TOSTRING(VERSION);
-#endif
 	p->website = "https://github.com/VCVRack/Tutorial";
 	p->manual = "https://github.com/VCVRack/Tutorial/blob/master/README.md";
 
